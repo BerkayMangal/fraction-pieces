31514b5f9b1143ddeecf8da8d02769bc4c331a3b
BerkayMangal <77927949+BerkayMangal@users.noreply.github.com>
2026-03-08 10:12:10 +0000

    Refactor levels array initialization

--- levels.js
+++ levels.js
@@ -1,21 +1,11 @@
 const levels = [
-
-{ order:["cheese","cheese","cheese","cheese"] },
-
-{ order:["cheese","cheese","olive","olive"] },
-
-{ order:["cheese","olive","olive","cheese"] },
-
-{ order:["cheese","cheese","cheese","olive"] },
-
-{ order:["pepperoni","pepperoni","cheese","cheese"] },
-
-{ order:["olive","olive","pepperoni","pepperoni"] },
-
-{ order:["cheese","olive","pepperoni","cheese"] },
-
-{ order:["pepperoni","olive","cheese","olive"] },
-
-{ order:["cheese","pepperoni","olive","cheese"] }
-
-]
+  { order: ["cheese", "cheese", "cheese", "cheese"] },
+  { order: ["cheese", "cheese", "olive", "olive"] },
+  { order: ["cheese", "olive", "olive", "cheese"] },
+  { order: ["cheese", "cheese", "cheese", "olive"] },
+  { order: ["pepperoni", "pepperoni", "cheese", "cheese"] },
+  { order: ["olive", "olive", "pepperoni", "pepperoni"] },
+  { order: ["cheese", "olive", "pepperoni", "cheese"] },
+  { order: ["pepperoni", "olive", "cheese", "olive"] },
+  { order: ["cheese", "pepperoni", "olive", "cheese"] }
+];
