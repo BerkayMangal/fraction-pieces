8974c86d01ca3e40b6dfeb46031f14e92fc850b1
BerkayMangal <77927949+BerkayMangal@users.noreply.github.com>
2026-03-08 09:38:51 +0000

    Add levels.js with predefined order arrays

---
+++ levels.js
@@ -0,0 +1,23 @@
+const levels = [
+
+{
+order:["cheese","cheese","cheese","cheese"]
+},
+
+{
+order:["cheese","cheese","olive","olive"]
+},
+
+{
+order:["cheese","olive","olive","cheese"]
+},
+
+{
+order:["cheese","cheese","cheese","olive"]
+},
+
+{
+order:["pepperoni","pepperoni","cheese","cheese"]
+}
+
+]
